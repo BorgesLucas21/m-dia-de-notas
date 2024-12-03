b2ebba4440281eac21029bb4deac5680d0fc6552
BorgesLucas21 <141366250+BorgesLucas21@users.noreply.github.com>
2024-12-03 09:28:36 -0300

    15

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('formulario').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    let nota1 = parseFloat(Document.getElementById('nota1').value);
-    let nota2 = parseFloat(Document.getElementById('nota2').value);
-    let nota3 = parseFloat(Document.getElementById('nota3').value);
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    let nota3 = parseFloat(document.getElementById('nota3').value);
 
     let media = (nota1 + nota2 + nota3) / 3;
 
